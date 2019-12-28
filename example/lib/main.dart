01a85882eb1f02b48741e9774133d3367738e78c
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-12-28 15:49:43 +0100

    Add sentry for Dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_sentry/flutter_sentry.dart';
 
-void main() => runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Sentry plugin example app'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text('Cause a native crash'),
-            onPressed: FlutterSentry.nativeCrash,
+void main() => FlutterSentry.wrap(
+      () async => runApp(MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Sentry plugin example app'),
+          ),
+          body: Column(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Cause a native crash'),
+                onPressed: FlutterSentry.nativeCrash,
+              ),
+              RaisedButton(
+                child: Text('Throw uncaught exception'),
+                onPressed: () => throw Exception('Uncaught exception'),
+              ),
+            ],
           ),
         ),
-      ),
-    )
-);
+      )),
+      dsn: 'https://420a0b0766e9450fbd3a456346c6eed2@sentry.io/1867468',
+    );
--- lib/flutter_sentry.dart
+++ lib/flutter_sentry.dart
@@ -1,9 +1,49 @@
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sentry/sentry.dart';
 
 class FlutterSentry {
   static const MethodChannel _channel = const MethodChannel('flutter_sentry');
 
   static Future<void> nativeCrash() => _channel.invokeMethod('nativeCrash');
+
+  static Future<T> wrap<T>(Future<T> Function() f, {@required String dsn}) {
+    final sentry = SentryClient(dsn: dsn);
+    return runZoned<Future<T>>(() async {
+      // This is necessary to initialize Flutter method channels so that
+      // our plugin can call into the native code. It also must be in the same
+      // zone as the app: https://github.com/flutter/flutter/issues/42682.
+      WidgetsFlutterBinding.ensureInitialized();
+
+      FlutterError.onError = (details) {
+        FlutterError.dumpErrorToConsole(details);
+        sentry.captureException(
+          exception: details.exception,
+          stackTrace: details.stack,
+        );
+      };
+
+      Isolate.current.addErrorListener(RawReceivePort((pair) async {
+        final List<String> errorAndStacktrace = pair;
+        debugPrint('Uncaught error in Flutter isolate: $errorAndStacktrace');
+        await sentry.captureException(
+          exception: errorAndStacktrace.first,
+          stackTrace: errorAndStacktrace.last == null
+              ? null
+              : StackTrace.fromString(errorAndStacktrace.last),
+        );
+      }).sendPort);
+
+      return await f();
+    }, onError: (exception, stackTrace) {
+      debugPrint('Uncaught error in zone: $exception\n$stackTrace');
+      sentry.captureException(
+        exception: exception,
+        stackTrace: stackTrace,
+      );
+    });
+  }
 }
