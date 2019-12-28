45cf07922b9186d2698022aa61bce183b7ed195e
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-12-28 19:57:57 +0100

    Move Sentry initialization to plugin
    Instead of an example app where it never belonged :)

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,38 +1,13 @@
 import UIKit
 import Flutter
-import Sentry
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    private let SentryDSNKey = "SentryDSN";
-
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        let dsn = Bundle.main.object(forInfoDictionaryKey: SentryDSNKey);
-        if (dsn == nil) {
-            NSException.raise(NSExceptionName.invalidArgumentException,
-                              format:"The value for key %@ is not set in Info.plist",
-                              arguments: getVaList([SentryDSNKey]))
-            return false;
-        }
-
-        let dsnString = dsn as? String;
-        if (dsnString == nil) {
-            NSException.raise(NSExceptionName.invalidArgumentException,
-                              format:"The value for key %@ is not a <string> type: %@",
-                              arguments: getVaList([SentryDSNKey, String(describing: dsn)]))
-        }
-
-        do {
-            Client.shared = try Client(dsn: dsnString!)
-            try Client.shared?.startCrashHandler()
-        } catch let error {
-            print("Failed to initialize Sentry: \(error)")
-        }
-
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
--- ios/Classes/SwiftFlutterSentryPlugin.swift
+++ ios/Classes/SwiftFlutterSentryPlugin.swift
@@ -3,13 +3,55 @@ import UIKit
 import Sentry
 
 public class SwiftFlutterSentryPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_sentry", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterSentryPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    private static let SentryDSNKey = "SentryDSN"
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    Client.shared?.crash()
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_sentry", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterSentryPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    override init() {
+        if Client.shared != nil {
+            return;
+        }
+
+        let dsn = Bundle.main.object(forInfoDictionaryKey: SwiftFlutterSentryPlugin.SentryDSNKey)
+        if dsn == nil {
+            NSException.raise(NSExceptionName.invalidArgumentException,
+                              format:"The value for key %@ is not set in Info.plist",
+                              arguments: getVaList([
+                                Self.SentryDSNKey,
+                              ]))
+        }
+
+        let dsnString = dsn as? String
+        if dsnString == nil {
+            NSException.raise(NSExceptionName.invalidArgumentException,
+                              format:"The value for key %@ is not a <string> type: %@",
+                              arguments: getVaList([
+                                Self.SentryDSNKey,
+                                String(describing: dsn),
+                              ]))
+        }
+
+        do {
+            Client.shared = try Client(dsn: dsnString!)
+            try Client.shared?.startCrashHandler()
+        } catch let error {
+            print("Failed to initialize Sentry: \(error)")
+        }
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        // Right now the only method we handle is nativeCrash(), so don't even bother checking.
+        // In future we may extend this API with user and context settings.
+        if let client = Client.shared {
+            client.crash();
+        } else {
+            result(FlutterError(code: "UNAVAILABLE",
+                                message: "Sentry shared client has not been initialized",
+                                details: nil))
+        }
+    }
 }
